ce078d6c1173d539be3adf83835423519685bdf5
CarlosVilche <carlitosviche@gmail.com>
2020-10-30 11:48:16 -0600

    Prueba de proyecto Vue.js

---
+++ src/components/BarraNavegacion.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item href="#">Home</b-nav-item>
+
+      <!-- Navbar dropdowns -->
+      <b-nav-item-dropdown text="Lenguaje" right>
+        <b-dropdown-item href="#">EN</b-dropdown-item>
+        <b-dropdown-item href="#">ES</b-dropdown-item>
+        <b-dropdown-item href="#">RU</b-dropdown-item>
+        <b-dropdown-item href="#">FA</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+      <b-nav-item-dropdown text="Login" right>
+        <b-dropdown-item href="#">Invitado</b-dropdown-item>
+        <b-dropdown-item href="#">Usuario</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+  </b-navbar>
+</div>
+    
+</template>
